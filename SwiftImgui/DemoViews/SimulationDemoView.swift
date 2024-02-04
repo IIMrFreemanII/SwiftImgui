084701e8bac8e30371c8ae344459b45d2db923e8
mykola.diahovets <mykola.diahovets@digicert.com>
2024-02-04 12:46:28 +0200

    test simulation

--- SwiftImgui/DemoViews/SimulationDemoView.swift
+++ SwiftImgui/DemoViews/SimulationDemoView.swift
@@ -20,12 +20,55 @@ func sdCircle(p: float2, r: Float) -> Float {
 }
 
 class SimulationDemoView : ViewRenderer {
-  static var width = 26;
-  static var height = 26;
+  static let divider = 4;
+  static var width = 28;
+  static var height = 28;
   var pixels = [Float](repeating: 0, count: width * height);
   
   var position = float2();
   var radius = Float(0.1);
+  
+  var center = int2();
+  var dir = float2(0, 1);
+  var deltaDir = float2();
+  
+  func calcFieldDir() {
+    var points: [int2] = [
+      center &+ int2(-1, 1),  center &+ int2(0, 1),  center &+ int2(1, 1),
+      center &+ int2(-1, 0),  center,                center &+ int2(1, 0),
+      center &+ int2(-1, -1), center &+ int2(0, -1), center &+ int2(1, -1),
+    ]
+    
+    var globalDir = float2()
+    var contributeCount = 0;
+    for point in points {
+      if (point.x >= Self.width || point.x < 0 || point.y >= Self.height || point.y < 0) {
+        continue
+      }
+      
+      let index = from2DTo1D(point: point, width: Self.width)
+      let centerIndex = from2DTo1D(point: center, width: Self.width)
+      
+      let value = pixels[index]
+      let centerValue = pixels[centerIndex]
+      let diff = max(0, centerValue - value)
+      if diff > 0 {
+        contributeCount += 1
+        
+        let dir = point &- center
+        globalDir += float2(Float(dir.x), Float(dir.y)) * diff
+      }
+    }
+    
+    var result = normalize(globalDir)
+    if (result.x.isNaN || result.y.isNaN) {
+      result = float2();
+    }
+    self.deltaDir = globalDir
+    print(globalDir)
+    print(contributeCount)
+    self.dir = result
+  }
   
   override func start() {
     for y in 0..<Self.height {
@@ -44,7 +87,18 @@ class SimulationDemoView : ViewRenderer {
         pixels[index] = color.clamped(to: 0...1);
       }
     }
-    print("");
+    
+    //----------
+    
+//    for y in 0..<3 {
+//      for x in 0..<3 {
+//        let point = int2(x, y);
+//        let index = from2DTo1D(point: point, width: 3);
+//
+//
+//        let value = arr[index]
+//      }
+//    }
   }
   
   override func draw(in view: MTKView) {
@@ -67,9 +121,31 @@ class SimulationDemoView : ViewRenderer {
           temp.mouseDown {
             print(pixels[index])
           }
+          temp.mouseOver {
+            center = int2(x: x, y: y)
+            print("Center: \(center)")
+            calcFieldDir()
+          }
           rect(temp, style: RectStyle(color: color))
         }
       }
+      
+      let start = float2(Float(center.x), Float(center.y)) * 25 + offset + float2(25, 25) * 0.5;
+      let end = start + self.dir * 30;
+      line(start, end, .red, 4)
+      
+//      for y in 0..<(Self.height / Self.divider) {
+//        for x in 0..<(Self.width / Self.divider) {
+//          let index = from2DTo1D(point: int2(x, y), width: Self.width / Self.divider);
+//          var color: Color = .blue;
+//          var position = float2(Float(x) * 25 * Float(Self.divider), Float(y) * 25 * Float(Self.divider)) + offset + (size + offset) * 1.5;
+//          let temp = Rect(position: position, size: size);
+////          temp.mouseDown {
+////            print(pixels[index])
+////          }
+//          rect(temp, style: RectStyle(color: color))
+//        }
+//      }
     }
   }
 }
